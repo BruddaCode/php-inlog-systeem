66356babe38fe13d0d805562537bc1797476693b
BruddaCode <Fabio.wolthuis@gmail.com>
2023-02-09 12:30:12 +0100

    index(home)+css

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Inlog Systeem</title>
+    <link rel="stylesheet" type="text/css" href="home.css">
+    <link rel="shortcut icon" href=https://imgs.search.brave.com/_416vqX3n8DoXjvlABlde7mWyW3mvmbyQC0jneAbeLI/rs:fit:474:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5G/MEp0bnczUXZNbF9L/UjhURFVFa1ZnSGFI/YSZwaWQ9QXBp>
+</head>
+<body class="homebody">
+    <div class="header">
+        <h1>Inlog Systeem</h1>
+    </div>
+    <div class="content">
+        <form method="post" action="login.php">
+            <table>
+                <tr>
+                    <td>Gebruikersnaam:</td>
+                    <td><input type="text" name="username" class="textInput"></td>
+                </tr>
+                <tr>
+                    <td>Wachtwoord:</td>
+                    <td><input type="password" name="password" class="textInput"></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><input type="submit" name="login_btn" value="Inloggen"></td>
+                </tr>
+            </table>
+        </form>
+    </div>
+</body>
+</html>
