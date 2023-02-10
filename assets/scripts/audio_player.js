679ca467ea6176ecf69b68fadb20c1ceb2848f3b
MVDW <maxvdwolf@gmail.com>
2023-02-10 10:09:41 +0100

    better audio player

--- assets/scripts/audio_player.js
+++ assets/scripts/audio_player.js
@@ -1,5 +1,22 @@
 var audio = new Audio("./assets/audio/ccp.mp3");
+audio.loop = true;
 
 audio.addEventListener("canplaythrough", (event) => {
 	audio.play();
 });
+
+document.body.addEventListener("click", (event) => {
+	if(!isPlaying ) audio.play();
+	
+
+});
+
+
+
+var isPlaying = function () {
+    return audio.currentAudio
+        && audio.currentAudio.currentTime > 0
+        && !audio.currentAudio.paused
+        && !audio.currentAudio.ended
+        && audio.currentAudio.readyState > 2;
+}
