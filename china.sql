f67baf4fed24c506654b66bd4e18a75de77c1e20
MVDW <maxvdwolf@gmail.com>
2023-02-13 09:00:40 +0100

    sql file

---
+++ china.sql
@@ -0,0 +1,81 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Feb 13, 2023 at 07:59 AM
+-- Server version: 5.7.41-0ubuntu0.18.04.1
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `china`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `game_data`
+--
+
+CREATE TABLE `game_data` (
+  `uid` int(11) NOT NULL,
+  `score` bigint(20) NOT NULL,
+  `item_1` int(11) NOT NULL,
+  `item_2` int(11) NOT NULL,
+  `item_3` int(11) NOT NULL,
+  `item_4` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `uid` int(6) NOT NULL,
+  `username` varchar(64) COLLATE utf8mb4_bin NOT NULL,
+  `password` varchar(64) COLLATE utf8mb4_bin NOT NULL,
+  `creation_date` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `game_data`
+--
+ALTER TABLE `game_data`
+  ADD PRIMARY KEY (`uid`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`uid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `uid` int(6) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
