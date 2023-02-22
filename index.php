d423ea4ff76dcd0276820c55583a96ccae5f4944
BruddaCode <Fabio.wolthuis@gmail.com>
2023-02-22 12:52:48 +0100

    error message

--- index.php
+++ index.php
@@ -66,50 +66,59 @@
 </head>
 
 <body class="homebody">
-    <div class="content" id="inlog-inhoudJS" >
     
-    	<?php if(isset($message)) echo $message; ?>
-    	
-        <form class="inlogform" action="index.php" method="post">
-            <img class="ccp" src="assets/images/ccp.png">
-            <br>
-            <br>
-            <label for="username">Username</label> <br>
-            <input class="inlog-field" type="text" name="username" required>
-            <br>
-            <br>
-            <label for="wachtwoord">Password</label> <br>
-            <input class="inlog-field" type="password" name="wachtwoord" required>
-            <br>
-            <br>
-            <input class="inlog-button" type="submit" value="Login" name="login-btn">
-            <br>
-            <p><a onclick="toRegister()">Click here to register</a></p>
-        </form>
+    <div class="grid">   
+        <div class="grid-item">
+            <?php 
+            if(isset($message)) 
+                echo '<p class="errorMessage">' . $message . '</p>';  
+            ?>
+        </div>
+        
+        <div class="grid-item">
+            <div class="content" id="inlog-inhoudJS" >
+                <img class="ccp" src="assets/images/ccp.png">
+                <form class="inlogform" action="index.php" method="post">
+                    <br>
+                    <label for="username">Username</label> <br>
+                    <input class="inlog-field" type="text" name="username" required>
+                    <br>
+                    <br>
+                    <label for="wachtwoord">Password</label> <br>
+                    <input class="inlog-field" type="password" name="wachtwoord" required>
+                    <br>
+                    <br>
+                    <input class="inlog-button" type="submit" value="Login" name="login-btn">
+                    <br>
+                    <p><a onclick="toRegister()">Click here to register</a></p>
+                </form>
+            </div>
+        
+            <div class="content" id="register-inhoudJS" style="display: none;">
+                <img class="ccp" src="assets/images/ccp.png">
+                <br>
+                <br>
+                <form class="registerform" action="index.php" method="post">
+                    <label for="regUsername">Username</label> <br>
+                    <input class="register-field" type="text" name="regUsername" required>
+                    <br>
+                    <br>
+                    <label for="regEmail">Email</label> <br>
+                    <input class="register-field" type="email" name="regEmail" required>
+                    <br>
+                    <br>
+                    <label for="regWachtwoord">Password</label> <br>
+                    <input class="register-field" type="text" name="regWachtwoord" required>
+                    <br>
+                    <br>
+                    <input class="register-button" type="submit" value="Register" name="reg-btn">
+                    <br>
+                    <p><a onclick="toLogin()">Click here to login</a></p>
+                </form>
+            </div>
+        </div>     
     </div>
     
-    <div class="content" id="register-inhoudJS" style="display: none;">
-        <img class="ccp" src="assets/images/ccp.png">
-        <br>
-        <br>
-        <form class="registerform" action="index.php" method="post">
-            <label for="regUsername">Username</label> <br>
-            <input class="register-field" type="text" name="regUsername" required>
-            <br>
-            <br>
-            <label for="regEmail">Email</label> <br>
-            <input class="register-field" type="email" name="regEmail" required>
-            <br>
-            <br>
-            <label for="regWachtwoord">Password</label> <br>
-            <input class="register-field" type="text" name="regWachtwoord" required>
-            <br>
-            <br>
-            <input class="register-button" type="submit" value="Register" name="reg-btn">
-            <br>
-            <p><a onclick="toLogin()">Click here to login</a></p>
-        </form>
-    </div>
 
     <script src="./assets/scripts/audio_player.js"></script> 
 
